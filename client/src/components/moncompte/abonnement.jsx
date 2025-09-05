c7e98d2385517a8b69634c14af7cafcaa5706516
laural62 <laisne.laura62@gmail.com>
2025-09-05 13:32:22 +0200

    maj abonnement page

--- client/src/components/moncompte/abonnement.jsx
+++ client/src/components/moncompte/abonnement.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export default function Abonnement() {
   return (
-    <div>Abonnement</div>
-  )
+    <section className="bg-black text-white h-screen">
+      <h2>Abonnement</h2>
+      <div>
+        <p>Pour avoir accés à d'autres surprise, souscrit un abonnement et découvre des supers bonus.</p>
+      </div>
+    </section>
+  );
 }
